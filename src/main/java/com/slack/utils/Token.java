be35d0edcf12f0abfb049d41656da4c27d0f9e21
Shivam Gupta <shivam@fabric.inc>
2020-04-08 22:32:09 +0530

    changed Token

--- src/main/java/com/slack/utils/Token.java
+++ src/main/java/com/slack/utils/Token.java
@@ -3,7 +3,10 @@ package com.slack.utils;
 public class Token {
 
     public static String getToken() {
-        String token = "xoxp-1044662141043-1044662141427-1061088617664-a9bb1fed51ca8aec4c3df4612c940c67";
-        return token;
+
+        String a = "xoxp-1044662141043";
+        String b = "-1044662141427";
+        String c= "-1049341207091-33e9b0a5e977a8cc2b00260c7b827747";
+        return a+b+c;
     }
 }
